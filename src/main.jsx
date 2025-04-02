c41a332145eaa53a2480f314e9da95c0a4382eb1
caioestevao1000 <caioestevao1000@gmail.com>
2025-04-01 21:51:53 -0300

    arrumado o caminho para PaginaTarefa.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import PaginaTarefa from './paginas/paginaTarefa.jsx'
+import PaginaTarefa from './paginas/PaginaTarefa.jsx'
 
 const router = createBrowserRouter([
 
